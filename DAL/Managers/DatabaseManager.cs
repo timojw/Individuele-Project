d2fdc1dea6ecaebcbe1c6356d9a62ee9b8548e4a
timojw <timomaas25@gmail.com>
2022-01-07 21:23:41 +0100

    Fixed Connection String

--- DAL/Managers/DatabaseManager.cs
+++ DAL/Managers/DatabaseManager.cs
@@ -8,6 +8,8 @@ namespace DAL.Managers
     public class DatabaseManager
     {
         public string connectionString { get; } = "Data Source=DESKTOP-AM2TG3L;Initial Catalog=MarktplaatsDatabase;Integrated Security=True";
+        //public string connectionString { get; } = "Server=TIMO-DESKTOP;Database=MarktplaatsDatabase;Trusted_Connection=True;MultipleActiveResultSets=true"
+
     }
 }
 
